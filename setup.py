a91f4b2333b138b896166bda809fcef97cf4413e
treedy <richard.cui@sgs.com>
2024-05-15 16:05:36 +0800

    Added the find packages for setup.py and .cfg

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Richard_Cui on 2024/5/15 10:56.
-from setuptools import setup
-
-setup()
+from setuptools import setup, find_packages
+setup(
+          name='sft-data-generator',
+          version='0.0.1',
+          packages=find_packages(where='src'),
+          package_dir={'': 'src'}
+      )
